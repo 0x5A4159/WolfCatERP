a26772a244bf8f0d929f9f375fccaca4b829b5ea
0x5A4159 <zyates001@gmail.com>
2023-06-11 15:39:21 -0400

    Adding session framework and improving user creation

--- main.js
+++ main.js
@@ -6,23 +6,15 @@ const bodyparser = require('body-parser');
 const net = require('net');
 const fs = require('fs');
 const https = require('https');
-
 const app = express();
-
 app.set('view engine', 'ejs');
-
 serverport = process.env.SERVERPORT;
 serverip = process.env.SERVERIPADDR;
-
-console.log();
-
 const key = fs.readFileSync('./rsa/localhost.key');
 const cert = fs.readFileSync('./rsa/localhost.crt');
+const session = require('express-session');
 const server = https.createServer({ key: key, cert: cert }, app);
-
 let userStat = {};
-
-console.log(serverip, serverport);
 
 mongoose.connect('mongodb://127.0.0.1:27017/appdb')
     .then((result) => { console.log("DB connect on localhost:27017"); server.listen(serverport,serverip); })
@@ -32,6 +24,28 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.use(express.static('static'));
+
+const redirectToLogin = (req, res, next) => {
+    if (!req.session.userID) {
+        res.redirect('/signup')
+    }
+    else {
+        next();
+    }
+}
+
+
+app.use(session({
+    name: 'sid',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'someSecretValue',
+    cookie: {
+        maxAge: 10000,
+        sameSite: true,
+        secure: 'development'
+    }
+}));
 
 app.use((req, _, next) => {
     userRequest = req.protocol + '://' + req.get('host') + req.originalUrl;
@@ -60,6 +74,9 @@ app.get("/home", (_, res) => {
 app.get("/index", (_, res) => {
     res.redirect("/");
 });
+
+// add redirectToLogin to args to make the page redirect to signin page
+// i.e. app.get("/tasks"), redirectToLogin (req, res) => { ... })
 
 app.get("/tasks", (req, res) => {
     task.find({ complete: false })
@@ -126,6 +143,57 @@ app.get("/tasks/api/fetchAll", (_, res) => {
         });
 });
 
+app.get("/admin/stats", (req, res) => {
+    if (req.socket.remoteAddress === process.env.SERVERIPADDR) {
+        res.send(userStat);
+    }
+});
+
+app.post("/signup", async (req, res) => {
+    if (passwordValidator(req.body.userPass)) {
+        if (emailValidator(req.body.userEmail)) {
+            let isUserExist = await user.findOne({ userEmail: req.body.userEmail });
+            let maxUserID = await user.find().sort({ userID: -1 }).limit(1);
+
+            console.log(typeof maxUserID, maxUserID);
+
+            if (isUserExist === null) {
+                user.create({
+                    userName: req.body.userName,
+                    userEmail: req.body.userEmail,
+                    userPass: req.body.userPass,    // TO-DO: salt and hash this password with bcrypt or similar library
+                    userSession: "",
+                    userRole: 1,
+                    userID: 1
+                }).then((result) => {
+                    res.status(200).send({ "success": true});
+                }).catch((err) => {
+                    console.log("Couldn't create new member");
+                    res.status(500).send({ "success": false });
+                })
+            }
+            else {
+                res.send({ "success": false, "message": "Email already in use" });
+            }
+        }
+        else {
+            res.send({ "success": false, "message": "Issue with Email provided."});
+        }
+    }
+    else {
+        res.send({ "success": false, "message": "Issue with password provided."});
+    }
+});
+
+
+app.post("/signin", async (req, res) => {
+    let signinuser = await user.findOne({ userName: req.params.urlid });
+
+    console.log('signinuser = ', signinuser);
+
+    res.redirect('/'); // on complete sign in push to index
+})
+
 app.post('/tasks/api/delOne', (req, res) => {
     if (userStat[req.socket.remoteAddress][userRequest] > 5) {
         res.status(400).json({ success: false });
@@ -151,20 +219,6 @@ app.post('/tasks/api/delOne', (req, res) => {
         }
     }
 });
-
-app.get("/admin/stats", (req, res) => {
-    if (req.socket.remoteAddress === process.env.SERVERIPADDR) {
-        res.send(userStat);
-    }
-});
-
-app.post("/signin", async (req, res) => {
-    let signinuser = await searchFind(req.params.urlid);
-
-    console.log('signinuser = ', signinuser);
-
-    res.redirect('/'); // on complete sign in push to index
-})
 
 app.post('/tasks/api/addOne', (req, res) => {
     if (userStat[req.socket.remoteAddress][userRequest] > 5) {
@@ -196,10 +250,20 @@ app.use((req, res) => {
     res.status(404).render('404');
 });
 
-const searchFind = async (objFind) => {
-    return await user.findOne({ userName: objFind });
-};
+function passwordValidator(password) {
+    hasUpper = /[A-Z]/.test(password);
+    hasLower = /[a-z]/.test(password);
+    hasNumeric = /[0-9]/.test(password);
+    hasSpecial = /[(\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\,|\.|\/|\?|\:|\;|\'|\"|\[|\{|\]|\}|\\|\||\<|\>|\-|\_|\+|\=)]/.test(password);
+    hasLength = password.length >= 8;
+    return [hasUpper, hasLower, hasNumeric, hasSpecial, hasLength].every(Boolean);
+}
+
+function emailValidator(email) {
+    emailDomain = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+    return emailDomain
+}
 
 setInterval(() => {
     userStat = {};
-}, 30_000);
+}, 15_000);
--- models/userinfo.js
+++ models/userinfo.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
     userName: { type: String, required: true },
-    userSession: { type: String, required: true },
+    userSession: { type: String, required: false },
+    userPass: {type:String, requireed: true},
     userRole: { type: Number, required: true },
     userID: { type: Number, required: true },
     userEmail: {type: String, required: true}
-});
+}, { versionKey: false });
 
 const User = mongoose.model('User', userSchema);
 
