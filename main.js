5e7b482bb768d91f2bda7adbb5ddf8ea486dfdcd
0x5A4159 <zyates001@gmail.com>
2023-08-01 22:47:55 -0400

    Added same pagination structure for users

--- main.js
+++ main.js
@@ -185,10 +185,31 @@ app.get("/users/:userid", (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-    user.find({})
+/*    user.find({})
         .then((result) => {
             res.render('users', {result});
         })
+        */
+    if (!isNaN(req.query.pagenum) && req.query.pagenum >= 0) {
+        user.find().sort('createdate').limit(5).skip(req.query.pagenum * 5).then((result) => {
+            user.countDocuments().then((documentCount) => {
+                res.render('users', {
+                    users: { result },
+                    total: documentCount,
+                    curPage: parseInt(req.query.pagenum)
+                });
+            });
+        })
+            .catch((error) => {
+                console.log(error);
+                res.redirect('/404')
+            })
+    }
+    else {
+        res.redirect('/users?pagenum=0');
+    }
+
+
 })
 
 app.post('/api/uploadAvatar', async (req, res) => {
